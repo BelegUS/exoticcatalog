c7c993ad205e1663836068932b0f28e26ffd5053
BelegUS <belegussuicune@gmail.com>
2014-08-17 23:41:22 +0200

    Crawler Tests

--- src/ExoticCarParts/CrawlerBundle/Controller/DefaultController.php
+++ src/ExoticCarParts/CrawlerBundle/Controller/DefaultController.php
@@ -3,11 +3,29 @@
 namespace ExoticCarParts\CrawlerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Goutte\Client;
 
-class DefaultController extends Controller
-{
-    public function indexAction($name)
-    {
-        return $this->render('CrawlerBundle:Default:index.html.twig', array('name' => $name));
+
+class DefaultController extends Controller {
+
+    public function indexAction() {
+
+    $url_to_traverse = 'http://www.ricambiamerica.com/parts_catalogs.php';
+
+    $client = new Client();
+    $crawler = $client->request('GET', $url_to_traverse);
+
+    $status_code = $client->getResponse()->getStatus();
+    if($status_code==200){
+        $data = array();
+        $crawler->filterXPath("//table[@id='catalogTable']//a[@href]")->each(function($node, $i) use($data) {
+                $data[$i] = $node->text();
+        });
     }
+    else{
+        $data='No Kitten :(';
+    }
+        return $this->render('CrawlerBundle:Default:index.html.twig', array('data' => $data));
+    }
+
 }
